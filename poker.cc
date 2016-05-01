f89c1113bb0bfdbb737529c041b840d92d269faf
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2016-05-01 14:39:42 +0300

    Switch to C++

--- poker.cc
+++ poker.cc
@@ -2,6 +2,10 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#define MAKE_CARD(v, s) (((s)<<4) + (v))
+#define CV(c) ((c)&15)
+#define CS(c) ((c)>>4)
+
 int next_values(uint8_t *hand, int len) {
     int i;
     for(i=0; i<len && ++hand[i] >= 15;  i++)
@@ -11,24 +15,8 @@ int next_values(uint8_t *hand, int len) {
 
 int main() {
     uint8_t hand[6] = { 2, 2, 2, 2, 2, 14 };
-    int i, j, c, N=3;
+    int i, j, c, N=5;
 
-    for(i=0; i<0xF0000; i++) {
-        c = 0;
-        for(j=0; j<5*4; j+=4) {
-            if(((i>>j)&15)<2 || ((i>>j)&15)>14) {
-                c = 1;
-                break;
-            }
-            if(((i>>j)&15) < ((i>>(j+4))&15)) {
-                c = 1;
-                break;
-            }
-        }
-        if(c) continue;
-        //printf("%05X\n", i);
-    }
-    return 0;
     do {
         for(i=0; i<N; i++)
             printf("%d ", hand[i]);
